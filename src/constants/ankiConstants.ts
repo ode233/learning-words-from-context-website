25d07b941fbb45001ba2a081f4fe18c03ac6e6ae
ode233 <ody122333@gmail.com>
2023-03-30 00:28:39 +0800

    update anki card

--- src/constants/ankiConstants.ts
+++ src/constants/ankiConstants.ts
@@ -8,7 +8,7 @@ export const ANKI_CARD_FRONT_HTML = `
     <section>{{Remark}}<section>
 {{/Remark}}
 
-<section class="srcImg">
+<section>
 {{Img}}
 <div class="source">
 {{PageIcon}}
@@ -32,7 +32,7 @@ export const ANKI_CARD_BACK_HTML = `
     <section>{{Remark}}<section>
 {{/Remark}}
 
-<section class="srcImg">
+<section>
 {{Img}}
 <div class="source">
 {{PageIcon}}
@@ -49,75 +49,55 @@ export const ANKI_CARD_BACK_HTML = `
 // eslint-disable-next-line spellcheck/spell-checker
 export const ANKI_CARD_CSS = `
 .card {
-    font-family: arial;
-    font-size: 20px;
-    text-align: center;
-    color: #333;
-    background-color: white;
-  }
-  
-  a {
-    color: #5caf9e;
-  }
-  
-  input {
-    border: 1px solid #eee;
-  }
-  
-  section {
-    margin: 1em 0;
-  }
-  
-  .trans {
-    border: 1px solid #eee;
-    padding: 0.5em;
-  }
-  
-  .trans_title {
-    display: block;
-    font-size: 0.9em;
-    font-weight: bold;
-  }
-  
-  .trans_content {
-    margin-bottom: 0.5em;
-  }
-  
-  .cloze {
-    font-weight: bold;
-    color: #f9690e;
-  }
-  
-  .source {
-    margin: 0.5em 0;
-    position: relative;
-    font-size: .8em;
-  }
-  
-  .source img {
-    height: .7em;
-  }
-  
-  .source a {
-    text-decoration: none;
-  }
-  
-  .typeGood {
-    color: #fff;
-    background: #1EBC61;
-  }
-  
-  .typeBad {
-    color: #fff;
-    background: #F75C4C;
-  }
-  
-  .typeMissed {
-    color: #fff;
-    background: #7C8A99;
-  }
+  font-family: arial;
+  font-size: 20px;
+  text-align: center;
+  color: #333;
+  background-color: white;
+}
 
-  .source img {
-    width: inherit;
-  }
+input {
+  border: 1px solid #eee;
+}
+
+section {
+  margin: 1em 0;
+}
+
+.cloze {
+  font-weight: bold;
+  color: #f9690e;
+}
+
+.source {
+  margin: 0.5em 0;
+  position: relative;
+  font-size: .8em;
+}
+
+.source img {
+  width: inherit;
+  height: .8em;
+}
+
+.source a {
+  color: #5caf9e;
+  text-decoration: none;
+  word-wrap: break-word;
+}
+
+.typeGood {
+  color: #fff;
+  background: #1EBC61;
+}
+
+.typeBad {
+  color: #fff;
+  background: #F75C4C;
+}
+
+.typeMissed {
+  color: #fff;
+  background: #7C8A99;
+}
 `;
